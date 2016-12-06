6bd183dc1a88ee27f73ea077a676a3dd27a593a9
DJRockstar <digvijays18@gmail.com>
2016-12-05 21:20:17 -0800

    Updated Code

---
+++ app.js
@@ -0,0 +1,124 @@
+var quiz = 
+	[
+     {image: "images/a_lincoln.jpg", 
+	  answer: 1,
+	  options:['George Bush', 'Abraham Lincoln','Thomas Jefferson', 'Kanye West'],
+	  },
+	 {image: "images/b_obama.jpg", 
+	  answer: 1,
+	  options:['George Bush', 'Barack Obama','Thomas Jefferson', 'Donald Trump']
+	  },
+	 {image: "images/g_ford.jpg", 
+	  answer: 2,
+	  options:['Harry Truman', 'Ben Franklin','Gerald Ford', 'Richard Nixon']
+	  },
+	 {image: "images/t_jefferson.jpg", 
+	  answer: 2,
+	  options:['Woodrow Wilson', 'Richard Pierce','Thomas Jefferson', 'Kanye West']
+	  },
+	 {image: "images/g_cleveland.jpg", 
+	  answer: 1,
+	  options:['George Bush', 'Grover Cleveland','Thomas Jefferson', 'Theodore Roosevelt']
+	  },		  		  
+	 {image: "images/j_kennedy.jpg", 
+	  answer: 1,
+	  options:['George Bush', 'John F Kennedy','Thomas Jefferson', 'Kanye West']
+	  },
+	 {image: "images/nixon.jpg", 
+	  answer: 1,
+	  options:['George Bush', 'Richard Nixon','Thomas Jefferson', 'Hilary Clinton']
+	  },
+	 {image: "images/g_washington.jpg", 
+	  answer: 1,
+	  options:['George Bush', 'George Washington','Thomas Jefferson', 'Kanye West']
+	  },
+	 {image: "images/f_pierce.jpg", 
+	  answer: 3,
+	  options:['George Bush', 'Barack Obama','Thomas Jefferson', 'Franklin Pierce']
+	  },
+   	 {image: "images/t_roosevelt.jpg", 
+	  answer: 1,
+	  options:['George Bush', 'Thomas Roosevelt','Thomas Jefferson', 'Kanye West']
+	 }
+	];
+
+var state = {
+	currentIndex : 0,
+	currentScore : 0
+};
+
+//Functions that modify state
+
+function updateScore(){		
+	state.currentScore += 1;
+}
+
+
+function checkAnswer(state, data, userChoice){
+	if(userChoice === data[index].answer){
+		updateScore();
+	}
+}
+
+
+
+//Functions that renders state on html
+
+function displayImage(state, data, index){
+	var image = data[index].image;
+	$('div.question-box').find('.img-display').attr('src',image);
+}
+
+
+function updateScoreValue(state, element){ //element - $('.score-section').find('.current-score')
+	var newScore = parseInt($('.current-score').text()) + 1;
+	element.html(newScore);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
